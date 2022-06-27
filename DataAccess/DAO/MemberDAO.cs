5723f5620093d528a0d22e8e9911036342cd4012
Man <mantvse150059@fpt.edu.vn>
2022-06-27 20:35:41 +0700

    first commit

---
+++ DataAccess/DAO/MemberDAO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class MemberDAO
+    {
+        public String GetConnectionString()
+        {
+            String connectionString;
+            IConfiguration config = new ConfigurationBuilder()
+                                    .SetBasePath(Directory.GetCurrentDirectory())
+                                    .AddJsonFile("appsettings.json", true, true).Build();
+            return config.GetConnectionString("DefaultConnection");
+
+        }
+
+    }
+}
