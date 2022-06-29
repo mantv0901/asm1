fbec72dfdafba2abb0f0b7d34dc04e2022eeed41
NhaPhan053 <thosanquy053@gmail.com>
2022-06-29 22:33:07 +0700

    Winform admin creat, update, delete

---
+++ Asm1/frmCreateForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Asm1
+{
+    partial class frmCreateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbCountry = new System.Windows.Forms.Label();
+            this.lbCity = new System.Windows.Forms.Label();
+            this.lbCompanyName = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbCompanyName = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.tbCountry = new System.Windows.Forms.TextBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.lbMemberID = new System.Windows.Forms.Label();
+            this.tbMemberID = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbEmail.Location = new System.Drawing.Point(426, 65);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(58, 25);
+            this.lbEmail.TabIndex = 0;
+            this.lbEmail.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(321, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(329, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 2;
+            // 
+            // lbCountry
+            // 
+            this.lbCountry.AutoSize = true;
+            this.lbCountry.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbCountry.Location = new System.Drawing.Point(426, 173);
+            this.lbCountry.Name = "lbCountry";
+            this.lbCountry.Size = new System.Drawing.Size(79, 25);
+            this.lbCountry.TabIndex = 3;
+            this.lbCountry.Text = "Country";
+            // 
+            // lbCity
+            // 
+            this.lbCity.AutoSize = true;
+            this.lbCity.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbCity.Location = new System.Drawing.Point(426, 121);
+            this.lbCity.Name = "lbCity";
+            this.lbCity.Size = new System.Drawing.Size(44, 25);
+            this.lbCity.TabIndex = 4;
+            this.lbCity.Text = "City";
+            // 
+            // lbCompanyName
+            // 
+            this.lbCompanyName.AutoSize = true;
+            this.lbCompanyName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbCompanyName.Location = new System.Drawing.Point(16, 170);
+            this.lbCompanyName.Name = "lbCompanyName";
+            this.lbCompanyName.Size = new System.Drawing.Size(142, 25);
+            this.lbCompanyName.TabIndex = 5;
+            this.lbCompanyName.Text = "CompanyName";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbPassword.Location = new System.Drawing.Point(16, 118);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(91, 25);
+            this.lbPassword.TabIndex = 6;
+            this.lbPassword.Text = "Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(361, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 15);
+            this.label8.TabIndex = 7;
+            // 
+            // tbCompanyName
+            // 
+            this.tbCompanyName.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbCompanyName.Location = new System.Drawing.Point(164, 167);
+            this.tbCompanyName.Name = "tbCompanyName";
+            this.tbCompanyName.Size = new System.Drawing.Size(250, 33);
+            this.tbCompanyName.TabIndex = 8;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbPassword.Location = new System.Drawing.Point(164, 118);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(250, 33);
+            this.tbPassword.TabIndex = 9;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbEmail.Location = new System.Drawing.Point(520, 62);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(250, 33);
+            this.tbEmail.TabIndex = 10;
+            // 
+            // tbCity
+            // 
+            this.tbCity.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbCity.Location = new System.Drawing.Point(520, 118);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(250, 33);
+            this.tbCity.TabIndex = 11;
+            // 
+            // tbCountry
+            // 
+            this.tbCountry.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbCountry.Location = new System.Drawing.Point(520, 167);
+            this.tbCountry.Name = "tbCountry";
+            this.tbCountry.Size = new System.Drawing.Size(250, 33);
+            this.tbCountry.TabIndex = 12;
+            // 
+            // btSave
+            // 
+            this.btSave.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btSave.Location = new System.Drawing.Point(133, 240);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(88, 42);
+            this.btSave.TabIndex = 13;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btUpdate.Location = new System.Drawing.Point(321, 240);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(88, 42);
+            this.btUpdate.TabIndex = 15;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            this.btUpdate.Click += new System.EventHandler(this.btUpdate_Click);
+            // 
+            // lbMemberID
+            // 
+            this.lbMemberID.AutoSize = true;
+            this.lbMemberID.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbMemberID.Location = new System.Drawing.Point(16, 65);
+            this.lbMemberID.Name = "lbMemberID";
+            this.lbMemberID.Size = new System.Drawing.Size(101, 25);
+            this.lbMemberID.TabIndex = 16;
+            this.lbMemberID.Text = "MemberID";
+            // 
+            // tbMemberID
+            // 
+            this.tbMemberID.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbMemberID.Location = new System.Drawing.Point(164, 62);
+            this.tbMemberID.Name = "tbMemberID";
+            this.tbMemberID.Size = new System.Drawing.Size(250, 33);
+            this.tbMemberID.TabIndex = 17;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btDelete.Location = new System.Drawing.Point(579, 240);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(83, 42);
+            this.btDelete.TabIndex = 18;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // frmCreateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 308);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.tbMemberID);
+            this.Controls.Add(this.lbMemberID);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.tbCountry);
+            this.Controls.Add(this.tbCity);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbCompanyName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbCompanyName);
+            this.Controls.Add(this.lbCity);
+            this.Controls.Add(this.lbCountry);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbEmail);
+            this.Name = "frmCreateForm";
+            this.Text = "frmCreateForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbCountry;
+        private System.Windows.Forms.Label lbCity;
+        private System.Windows.Forms.Label lbCompanyName;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbCompanyName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.TextBox tbCountry;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Label lbMemberID;
+        private System.Windows.Forms.TextBox tbMemberID;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
---
+++ DataAccess/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataAccess.DAO;
+using BusinessLayer.Models;
+
+namespace DataAccess
+{
+    public class Class1
+    {
+        static void Main()
+        {
+            MemberDAO dao = new MemberDAO();
+            Member m  = new Member();
+            m.CompanyName = "1";
+            m.City = "TPHCM";
+            m.Email = "qhuy1911@gmail.com";
+            m.Country = "TPHCM";
+            m.Password = "123123";
+
+            bool rs = dao.Create(m);
+            Console.WriteLine(rs);
+        }
+    }
+}
